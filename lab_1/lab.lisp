4ce907c322be0e6482996ab98acd013e37085c89
Bugakov Ivan <bugakov0601@yandex.ru>
2025-04-07 20:10:59 +0300

    first ready

--- lab_1/lab.lisp
+++ lab_1/lab.lisp
@@ -13,3 +13,34 @@
 (print ((lambda (ar1 ar2 ar3 ar4) 
     (cons (cons ar1 (cons ar2 nil)) (cons (cons ar3 (cons ar4 nil)) nil))
 ) '(a b) '(c d) '(e f) '(g h)))
+
+(defun f2(ar1 ar2) 
+    (cons (cons ar1 nil) (cons (cons ar2 nil) nil))
+)
+
+(print (f2 'a 'b))
+(print (f2 '(a b) '(c d)))
+
+
+(print ((lambda (ar1 ar2) 
+    (cons (cons ar1 nil) (cons (cons ar2 nil) nil))
+) 'a 'b))
+
+(print ((lambda (ar1 ar2) 
+    (cons (cons ar1 nil) (cons (cons ar2 nil) nil))
+) '(a b) '(b c)))
+
+(defun f3(ar1) 
+    (cons (cons (cons ar1 nil) nil) nil)
+)
+
+(print (f3 'a))
+(print (f3 '(a b)))
+
+(print ((lambda (ar1) 
+    (cons (cons (cons ar1 nil) nil) nil)
+) 'a))
+
+(print ((lambda (ar1) (
+    cons (cons (cons ar1 nil) nil) nil)
+) '(a b)))
